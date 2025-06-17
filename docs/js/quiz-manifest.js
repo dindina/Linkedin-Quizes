afcdc5e217151eaad993a998f3a999f343c71a20
dinesh <dindina@gmail.com>
2025-06-17 17:56:27 +1000

    added more files

--- docs/js/quiz-manifest.js
+++ docs/js/quiz-manifest.js
@@ -1,6 +1,12 @@
 const allQuizzes = [
-    { title: "System Design Quiz", path: "./system-design/quiz1.html" },
-
+    {
+        "title": "Designing Ticket Master application",
+        "path": "./system-design/designing-ticket-master-application-2025-06-17-quiz.html"
+    },
+    {
+        "title": "System Design Quiz",
+        "path": "./system-design/quiz1.html"
+    }
 ];
 
 module.exports = { allQuizzes };
