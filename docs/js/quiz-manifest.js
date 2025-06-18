988b99f52d0e984f629d5da9584cc1b7fa0c0f09
dinesh <dindina@gmail.com>
2025-06-18 11:04:46 +1000

    added quiz mainfest

--- docs/js/quiz-manifest.js
+++ docs/js/quiz-manifest.js
@@ -2,6 +2,10 @@ const allQuizzes = [
     {
         "title": "CAP Theorem",
         "path": "./system-design/cap-theorem-2025-06-18-quiz.html"
+    },
+    {
+        "title": "Microservices vs. Monolith Architecture",
+        "path": "./system-design/microservices-vs.-monolith-architecture-2025-06-18-quiz.html"
     }
 ];
 
