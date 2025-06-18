b9cc7adbc85f8bc9d6a89edd3a41acb9bbf061bb
dinesh <dindina@gmail.com>
2025-06-18 11:15:52 +1000

    updated quiz manifest

--- docs/js/quiz-manifest.js
+++ docs/js/quiz-manifest.js
@@ -8,6 +8,10 @@ const allQuizzes = [
         "path": "./system-design/microservices-vs.-monolith-architecture-2025-06-18-quiz.html"
     },
     {
+        "title": "SQL Database Tuning",
+        "path": "./system-design/sql-database-tuning-2025-06-18-quiz.html"
+    },
+    {
         "title": "SQL vs NoSQL Databases",
         "path": "./system-design/sql-vs-nosql-databases-2025-06-18-quiz.html"
     }
