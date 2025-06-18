a045c9965f452a9b092260f6eaac01779f2a509e
dinesh <dindina@gmail.com>
2025-06-18 10:51:46 +1000

    added empty qui manifest

--- docs/js/quiz-manifest.js
+++ docs/js/quiz-manifest.js
@@ -1,16 +1,5 @@
 const allQuizzes = [
-    {
-        "title": "CAP Theorem",
-        "path": "./system-design/cap-theorem-2025-06-18-quiz.html"
-    },
-    {
-        "title": "Designing Ticket Master application",
-        "path": "./system-design/designing-ticket-master-application-2025-06-17-quiz.html"
-    },
-    {
-        "title": "System Design Quiz",
-        "path": "./system-design/quiz1.html"
-    }
+
 ];
 
 module.exports = { allQuizzes };
