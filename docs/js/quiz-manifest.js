4dd9c953074306060e54969470cfcb9ea16abbf6
dinesh <dindina@gmail.com>
2025-06-18 11:11:53 +1000

    added quiz manifest

--- docs/js/quiz-manifest.js
+++ docs/js/quiz-manifest.js
@@ -6,6 +6,10 @@ const allQuizzes = [
     {
         "title": "Microservices vs. Monolith Architecture",
         "path": "./system-design/microservices-vs.-monolith-architecture-2025-06-18-quiz.html"
+    },
+    {
+        "title": "SQL vs NoSQL Databases",
+        "path": "./system-design/sql-vs-nosql-databases-2025-06-18-quiz.html"
     }
 ];
 
